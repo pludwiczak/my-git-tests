d71cb5b6680b3252544329c8b717a1e03c240eac
piotr <3.14otrek@poczta.fm>
2018-12-01 11:36:23 +0100

    [add] - dodanie HelloWorld

---
+++ src/main/java/pl/sda/ludwiczak/project/my_git_test/HelloWorld.java
@@ -0,0 +1,9 @@
+package pl.sda.ludwiczak.project.my_git_test;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Helllllllllllllllloooooooooooooooooooooo");
+    }
+
+}
